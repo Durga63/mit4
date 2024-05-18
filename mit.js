51226e3140ba70da2ae1532b88fa0906ba2a2b2f
Durga63 <164005684+Durga63@users.noreply.github.com>
2024-05-18 11:30:18 +0530

    Add files via upload

---
+++ mit.js
@@ -0,0 +1,10 @@
+const contactForm = document.getElementById('contact-form');
+const contactInput = document.getElementById('contact-input');
+const contactDetails = document.getElementById('contact-details');
+
+
+function storedetails() {
+    var details = document.contactDetails("li")
+    details.textContent = contact-input.value
+    ul.append(details)
+}
